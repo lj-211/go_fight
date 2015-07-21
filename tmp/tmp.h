4186239c81c8def757a0c72e502f3791174de078
lj <lj@localhost.localdomain>
2015-07-21 10:49:37 +0800

    [add] 1.代码规范

--- tmp/tmp.h
+++ tmp/tmp.h
@@ -9,4 +9,33 @@
 //#include <log4cplus/spi/loggerimpl.h>
 //#include <log4cplus/spi/loggingevent.h>
 
+/*
+头文件包含书序
+#include "self_h_file"
+
+#include "项目内头文件"
+
+#include "第三方库头文件"
+
+#include <系统头文件>
+
+class SampleClass {
+public:
+    SampleClass();
+    explicit SampleClass(int val);
+
+private:
+    int member_int_;
+    string member_str_;
+};
+
+非类函数
+void this_is_c_like_fun();
+
+局部变量,如果重名概率较大
+int _scope_val;
+
+在CPP中,尽量使用匿名空间,少使用静态成员
+*/
+
 #endif
